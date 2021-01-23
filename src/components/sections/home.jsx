e94e01fae0d989420d544bf606c3868679168521
zhichaoSun <zhichao_sun@outlook.com>
2021-01-23 21:51:55 +0800

    add effects to home circles

--- src/components/sections/home.jsx
+++ src/components/sections/home.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { gsap, ScrollTrigger, Power3 } from "gsap/all";
 
 import C1 from "../../static/images/C1.svg"
 import C2 from "../../static/images/C2.svg"
@@ -6,6 +7,23 @@ import C3 from "../../static/images/C3.svg"
 import C4 from "../../static/images/C4.svg"
 
 const Home = () => {
+
+    useEffect(() => {
+        gsap.timeline()
+        .set(".circles", {
+            autoAlpha: 1,
+        })
+        .from(".circle", {
+            autoAlpha: 0,
+            scale: 0.6,
+            duration: 1,
+            xPercent: 150,
+            ease: Power3.easeInOut,
+            stagger: {
+                amount: 0.3,
+            }
+        })
+    }, [])
     return (
         <>
             <div className="divider" id="home"></div>
