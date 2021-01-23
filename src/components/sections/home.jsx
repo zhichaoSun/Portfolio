43a318db1e407e29d3eaa326f65598eb1e7dd2c2
zhichaoSun <zhichao_sun@outlook.com>
2021-01-24 00:18:59 +0800

    add tilt effect to home circles and content

--- src/components/sections/home.jsx
+++ src/components/sections/home.jsx
@@ -1,11 +1,28 @@
 import React, { useEffect } from 'react'
 import { gsap, ScrollTrigger, Power3 } from "gsap/all";
 
+import VanillaTilt from "vanilla-tilt";
+
 const Home = () => {
 
     useEffect(() => {
+
+        VanillaTilt.init(document.querySelector(".circles"), {
+            max: 5,
+            speed: 400,
+            reverse: true,
+            "full-page-listening": true
+        })
+
+        VanillaTilt.init(document.querySelector(".content"), {
+            max: 5,
+            speed: 1000,
+            reverse: true,
+            "full-page-listening": true
+        })
+
         gsap.timeline()
-        .set(".circles", {
+        .set([".circles", ".content"], {
             autoAlpha: 1,
         })
         .from(".circle", {
@@ -19,13 +36,34 @@ const Home = () => {
                 from: "end"
             }
         })
+        .to(".circle1", {
+            z: 150,
+        }, "<")
+        .to(".circle2", {
+            z: 110,
+        }, "<")
+        .to(".circle3", {
+            z: 70,
+        }, "<")
+        .to(".circle4", {
+            z: 30,
+        }, "<")
+        .from(".content", {
+            autoAlpha: 0,
+            duration: 1,
+            xPercent: -150,
+            ease: Power3.easeInOut,
+        }, "<")
     }, [])
+
+    
+
     return (
         <>
             <div className="divider" id="home"></div>
             <section className="home" >
                 <div className="content">
-                    Home Content
+                    <p className="name">SUN</p>
                 </div>
                 <div className="circles">
                     <div className="circle circle4"></div>
