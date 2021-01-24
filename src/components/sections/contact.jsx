43a3fbd6e069c512310c1d01c3d716050403e811
zhichaoSun <zhichao_sun@outlook.com>
2021-01-24 17:14:20 +0800

    contact section: circles and rects

--- src/components/sections/contact.jsx
+++ src/components/sections/contact.jsx
@@ -5,39 +5,29 @@ import * as yup from "yup"
 
 import { TextField, Button } from "@material-ui/core";
 
-import C1 from "../../static/images/C1.svg"
-import C3 from "../../static/images/C3.svg"
-import R2 from "../../static/images/R2.svg"
-import R4 from "../../static/images/R4.svg"
-
 const Contact = () => {
     return (
         <>
             <div className="divider" id="contact"></div>
-            <section id="contact">
+            <section className="contact">
                 <div className="contact-grid-container">
                     <div className="contactL">
-                        <div className="circle">
-                            <img src={C3} alt="C3"/>
-                        </div>
-                        <div className="rect">
-                            <img src={R2} alt="R2"/>
-                        </div>
+                        <div className="rectL"></div>
+                        <div className="circleL"></div>
                     </div>
 
                     <div className="contactForm">
-                        <div className="from">
+                        <div className="title">
                             <h1>Contact <span>Me</span></h1>
+                        </div>
+                        <div className="form">
+                            123
                         </div>
                     </div>
 
                     <div className="contactR">
-                        <div className="rect">
-                            <img src={R4} alt="R4"/>
-                        </div>
-                        <div className="circle">
-                            <img src={C1} alt="C1"/>
-                        </div>
+                        <div className="circleR"></div>
+                        <div className="rectR"></div>
                     </div>
                 </div>
             </section>
